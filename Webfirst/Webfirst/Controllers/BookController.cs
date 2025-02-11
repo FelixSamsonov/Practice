974df9ed97cab68ad2221b6e6e22b68ee4d8270a
Felix Samsonov <german1samsonov@gmail.com>
2025-02-11 08:32:30 +0200

    Web

---
+++ Webfirst/Webfirst/Controllers/BookController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Webfirst.Data;
+using Webfirst.Models;
+
+namespace Webfirst.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Book
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Books.ToListAsync());
+        }
+
+        // GET: Book/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parametrOfBook = await _context.Books
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (parametrOfBook == null)
+            {
+                return NotFound();
+            }
+
+            return View(parametrOfBook);
+        }
+
+        // GET: Book/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Book/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Author,Genre,Year,Pages,Language")] ParametrOfBook parametrOfBook)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(parametrOfBook);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(parametrOfBook);
+        }
+
+        // GET: Book/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parametrOfBook = await _context.Books.FindAsync(id);
+            if (parametrOfBook == null)
+            {
+                return NotFound();
+            }
+            return View(parametrOfBook);
+        }
+
+        // POST: Book/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Author,Genre,Year,Pages,Language")] ParametrOfBook parametrOfBook)
+        {
+            if (id != parametrOfBook.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(parametrOfBook);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ParametrOfBookExists(parametrOfBook.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(parametrOfBook);
+        }
+
+        // GET: Book/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parametrOfBook = await _context.Books
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (parametrOfBook == null)
+            {
+                return NotFound();
+            }
+
+            return View(parametrOfBook);
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var parametrOfBook = await _context.Books.FindAsync(id);
+            if (parametrOfBook != null)
+            {
+                _context.Books.Remove(parametrOfBook);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ParametrOfBookExists(int id)
+        {
+            return _context.Books.Any(e => e.Id == id);
+        }
+    }
+}
